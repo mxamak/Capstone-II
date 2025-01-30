825c50b886bb85514d532c4b11faa11160e6f2a2
Mohammed Aqib <mohammedaqib@my.unt.edu>
2025-01-30 15:21:29 -0600

    added basic home , login, and registeration components

--- project-4902/src/app/app.component.ts
+++ project-4902/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { RegistrationComponent } from "./registration/registration.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, HomeComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
