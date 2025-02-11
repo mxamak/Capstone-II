de474732187bf53fd1e6016beac04bb0c9e31ed6
OzielPlatas <ozielplatas@gmail.com>
2025-02-11 10:56:33 -0600

    Added Research Creation Component

--- project-4902/src/app/app.routes.ts
+++ project-4902/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { ResearchCreationComponent } from './research-creation/research-creation.component';
 
 export const routes: Routes = [
     {'path': '', 'title':'Research Finder', component: HomeComponent},
     {'path': 'login', 'title':'login', component: LoginComponent},
     {'path': 'register', 'title':'register', component: RegistrationComponent},
+    { 'path': 'research-creation', component: ResearchCreationComponent }
 ];
