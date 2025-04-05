7dc6914419479c1454056ebfaeece6736ed98e2b
Gustavo Platas <116664229+GustavoPlatas22@users.noreply.github.com>
2025-04-04 22:03:47 -0500

    Deployment through Firebase Hosting

--- project-4902/src/app/app.routes.ts
+++ project-4902/src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ResearchCreationComponent } from './research-creation/research-creation.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
-import { OurMissionComponent } from './our-mission/our-mission.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ParticipantsResearchDetailsComponent } from './participants-research-details/participants-research-details.component';
@@ -18,23 +17,22 @@ import { ViewFeedbackComponent } from './view-feedback/view-feedback.component';
 import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
 
 export const routes: Routes = [
-  { path: '', title: 'Research Finder', component: HomeComponent },
-  { path: 'login', title: 'Login', component: LoginComponent },
-  { path: 'register', title: 'Register', component: RegistrationComponent },
-  { path: 'how-it-works', component: HowItWorksComponent },
-  { path: 'our-mission', component: OurMissionComponent },
-  { path: 'about-us', component: AboutUsComponent },
-  { path: 'contact-us', component: ContactUsComponent },
-  { path: 'additional-resources', component: AdditionalResourcesComponent},
-  { path: 'news', component: NewsComponent},
-  { path: 'view-profile', component: ViewProfileComponent},
-  { path: 'view-others-profile/:userId',  component: ViewOthersProfileComponent},
-  { path: 'your-projects', component: YourProjectsComponent},
-  { path: 'feedback-form/:projectId', component: FeedbackFormComponent },
-  { path: 'view-feedback/:projectId', component: ViewFeedbackComponent },
-  { path: 'create-research/:id', component: ResearchCreationComponent },
+  { path: '', title: 'Research Finder', component: HomeComponent, data: { renderMode: 'no-prerender' } },
+  { path: 'login', title: 'Login', component: LoginComponent, data: { renderMode: 'no-prerender' }},
+  { path: 'register', title: 'Register', component: RegistrationComponent, data: { renderMode: 'no-prerender' } },
+  { path: 'how-it-works', component: HowItWorksComponent, data: { renderMode: 'no-prerender' } },
+  { path: 'about-us', component: AboutUsComponent, data: { renderMode: 'no-prerender' } },
+  { path: 'contact-us', component: ContactUsComponent, data: { renderMode: 'no-prerender' } },
+  { path: 'additional-resources', component: AdditionalResourcesComponent, data: { renderMode: 'no-prerender' }},
+  { path: 'news', component: NewsComponent, data: { renderMode: 'no-prerender' }},
+  { path: 'view-profile', component: ViewProfileComponent, data: { renderMode: 'no-prerender' }},
+  { path: 'view-others-profile/:userId',  component: ViewOthersProfileComponent, data: { renderMode: 'no-prerender' }},
+  { path: 'your-projects', component: YourProjectsComponent, data: { renderMode: 'no-prerender' }},
+  { path: 'feedback-form/:projectId', component: FeedbackFormComponent, data: { renderMode: 'no-prerender' } },
+  { path: 'view-feedback/:projectId', component: ViewFeedbackComponent, data: { renderMode: 'no-prerender' } },
+  { path: 'create-research/:id', component: ResearchCreationComponent, data: { renderMode: 'no-prerender' } },
 
   // 🔐 Protected Routes
-  { path: 'research-creation', component: ResearchCreationComponent, canActivate: [AuthGuard] },
-  { path: 'participants-research-details', component: ParticipantsResearchDetailsComponent, canActivate: [AuthGuard] }
+  { path: 'research-creation', component: ResearchCreationComponent, canActivate: [AuthGuard], data: { renderMode: 'no-prerender' } },
+  { path: 'participants-research-details', component: ParticipantsResearchDetailsComponent, canActivate: [AuthGuard], data: { renderMode: 'no-prerender' } }
 ];
